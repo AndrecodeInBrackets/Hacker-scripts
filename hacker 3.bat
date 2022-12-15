07604dcbae1ddbfaa6072c2c36e2a8a9e260bf88
Andrecode <73338965+andrecodeV1@users.noreply.github.com>
2022-12-15 18:27:43 +0200

    Add files via upload

---
+++ hacker 3.bat
@@ -0,0 +1,11 @@
+@echo off
+
+color 0a
+
+:loop
+
+echo HACKED BY ANONYMOUS
+
+echo HAHAHAHAHAHA
+
+goto loop
---
+++ hacker.bat
@@ -0,0 +1,10 @@
+@echo off
+
+color 0a
+
+:loop
+
+echo %random% % % %random% % % %random% % % %random% % % %random% % % %random% % % %random% % % %random% % % %random% % % %random% % %
+
+goto loop
+
